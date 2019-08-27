0f127343d4eead4220420495ebe80d8f883e9936
Alice <effie127@gmail.com>
2019-08-26 21:26:31 -0700

    Added client name to header.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import Airtable from 'airtable';
+const base = new Airtable({ apiKey: 'keyCxnlep0bgotSrX' }).base('appN1J6yscNwlzbzq');
+
+const calendarHash = window.location.hash.slice(2, 16);
+let clientName = '';
 
 function Header() {
+
+  base('Calendars').select({
+    filterByFormula: `{hash}='${calendarHash}'`
+  }).eachPage((records) => {
+    clientName = records[0].fields['client'];
+  });
+
   return (
     <header id="header">
-      <img className="logo" src="images/ADURO-Logo-Horizontal.png" />
-      <h1 className="my-2">Custom Content Loader</h1>
+      <div className="row">
+        <div className="col-6 offset-3">
+            <img className="logo" src="images/ADURO-Logo-Horizontal.png" />
+            <h1 className="my-1">Custom Content Loader</h1>
+        </div>
+        <div className="col-3">
+            <p className="client-name">Client Name: {clientName}</p>
+        </div>
+      </div>
     </header>
   );
 }
